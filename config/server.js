cc1115e74d662c69b5aea018e316f74e43ef767b
Zakaria Kerkeb <hikkary@mac.local>
2023-06-15 22:40:45 +0100

    feat(report/subscription): in progress

--- config/server.js
+++ config/server.js
@@ -3,5 +3,6 @@ module.exports = ({ env }) => ({
   port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS', ['CAJNi/NfdlaRKvCN6i9QOg==,ONOUDJvX38LtELfWSf+iNg==,RdmrroJzzAvJVr2ZfRNqyg==,TtzyDM5B2s2QYoe+MsCdQw==']),
-  },
+  }
+ 
 });
