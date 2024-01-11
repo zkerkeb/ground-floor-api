f18f901c4cc240b7476b22eebc688c881c2abd85
Zakaria Kerkeb <hikkary@mac.local>
2024-01-11 05:26:43 +0100

    feat(articles): controller updated to populate

--- src/api/article/controllers/article.js
+++ src/api/article/controllers/article.js
@@ -1,9 +1,54 @@
-'use strict';
+// 'use strict';
 
-/**
- * article controller
- */
+// /**
+//  * article controller
+//  */
+
+// const { createCoreController } = require('@strapi/strapi').factories;
+
+// // faire en sorte de populate la relation related_articles
+
+
+
+// module.exports = createCoreController('api::article.article');
+'use strict';
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::article.article');
+module.exports = createCoreController('api::article.article', ({ strapi }) =>  ({
+  // Surcharge de la méthode find
+  async find(ctx) {
+    // Spécifiez les relations à peupler, y compris les relations imbriquées
+    ctx.query = {
+      ...ctx.query,
+      populate: {
+        related_articles: {
+          populate: ['illustration'], // Remplacez par les champs réels que vous voulez peupler
+        },
+      },
+    };
+
+    // Utilisez le comportement par défaut pour obtenir les données
+    const { data, meta } = await super.find(ctx);
+
+    return { data, meta };
+  },
+
+  // Surcharge de la méthode findOne
+  async findOne(ctx) {
+    // Spécifiez les relations à peupler, y compris les relations imbriquées
+    ctx.query = {
+      ...ctx.query,
+      populate: {
+        related_articles: {
+          populate: ['illustration'], // Remplacez par les champs réels que vous voulez peupler
+        },
+      },
+    };
+
+    // Utilisez le comportement par défaut pour obtenir les données
+    const { data } = await super.findOne(ctx);
+
+    return { data };
+  },
+}));
